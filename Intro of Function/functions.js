25cbf8bbb4fb62df5b6d8488b80428a63e259f43
Areeba Naeem <areebanaeem069@gmail.com>
2024-09-07 12:47:25 +0500

    Add files via upload

---
+++ Intro of Function/functions.js
@@ -0,0 +1,27 @@
+function display(name){
+
+    console.log(`hey! ${name}`);
+
+}
+
+function name(params) {
+    
+}
+// anonymous as well as expression function 
+const greet = (name)=>{
+    console.log(`hey ${name}`)
+}
+
+greet("ayesha");
+//
+const greet2 = function greetings (name){
+    console.log(`hey ${name}`)
+}
+
+greet("ayesha");
+
+// 
+setTimeout(function greetings(){
+console.log(`${"Hello"} ahsan`),3000
+
+});
