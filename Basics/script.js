b4dacc9467dc3a5d702a43b512fdf15dbe6962c3
Areeba Naeem <areebanaeem069@gmail.com>
2024-09-07 12:22:45 +0500

    Add files via upload

---
+++ Basics/script.js
@@ -0,0 +1,57 @@
+console.log("hello world");
+// alert("first code is her!");
+// prompt("blast");
+{
+    var a=4;
+
+}
+console.log(a);
+if (a==4){
+    let re=3;
+}
+// console.log(re);
+
+// var con=confirm("exist");
+var var1=null;
+let var2=undefined;
+const var3=undefined;
+
+console.log("var1:"+var1  );
+console.log(typeof (var1));
+
+console.log("var2:"+var2);
+console.log(typeof (var2));
+
+console.log("var3:"+var3);
+console.log(typeof (var3));
+
+let st="areeba"+1;
+console.log(typeof (st));
+
+switch(a){
+    case 4:
+        console.log("win");
+
+}
+
+
+//objects
+
+let obj={
+    name:"areeba",
+    age:20
+}
+console.log(obj);
+
+ const obj1={
+    name:"areeba",
+    age:20
+}
+
+obj1.age=10;
+obj1.salary=300000;
+
+console.log(obj1);
+
+
+
---
+++ Basics/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+var con=confirm("exist");
