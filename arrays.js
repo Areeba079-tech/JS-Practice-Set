8a8f4b01731a84a042dc761091fba584b1765fa3
Areeba Naeem <areebanaeem069@gmail.com>
2024-09-19 18:59:38 +0500

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -37,4 +37,52 @@ console.log("Original:"+arr)
 console.log("Splice: "+arr.splice(1,3))
 console.log("original:"+arr)
 
+// arrays part 2
+//difference between concatenate and push 
+// push manupulates the same array but concatenate returns new array 
+// if some array is push into an array then the pushing array will be considered as single element 
+
+const arr1=[1,2,3]
+const arr2=['a','b','c']
+
+// arr1.push(arr2)   // it will take arr2 as a single element 
+// console.log(arr1)
+// console.log(arr1[3][1])
+
+// concat
+// const arr3=arr1.concat(arr2)
+// console.log(arr3)
+
+
+// spread operator(...) : spread each element of the arrays into the new array 
+
+// const arr4=[...arr1,...arr2];
+// console.log(arr4)
+
+
+// concatenate has limitation that it will concatenate only 2 arrays at one time but spread operator can concatenate more than 2 arrays also
+
+// const arr5=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const flat_arr=arr5.flat(Infinity);  //infinity means depth of array : to how much instance we want to flat array
+// console.log(flat_arr)
+
+
+//checking dynamically if data coming in array ?
+let namee="Areeba"
+console.log(Array.isArray(namee))
+// converting anything in an array
+console.log(Array.from(namee))
+// for object 
+//==============intresting case==============
+console.log(Array.from({name:"Areeba"}))// "from" can't convert object directly into array 
+// we need to mention that create array of keys or create array of values , directly a whole object cannot be converted 
+
+//converting independent values into a single array 
+const var1="Areeba"
+const var2="aiman"
+const var3="riffat"
+
+console.log(Array.of(var1,var2,var3)
+)
+
 
