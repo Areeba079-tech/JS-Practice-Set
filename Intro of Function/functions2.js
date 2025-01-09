910cc6e11201fc0cb8e9dbb9d16d718c40b466e7
Areeba Naeem <areebanaeem069@gmail.com>
2025-01-09 23:38:31 +0500

    Update functions2.js

--- Intro of Function/functions2.js
+++ Intro of Function/functions2.js
@@ -23,3 +23,68 @@ console.log(loginUser())// undefined will be printed because something undefined
 
 //=================functions with objects================
  
+
+// use of rest operator to pass and return multiple(donot know the exact number of expected arguments to be passed) instances 
+// function calculateCartPrice(...num1){
+//     return num1;
+
+// }
+
+function calculateCartPrice(var1,var2,...num1){
+    // var1 =200 
+    // var2=400 
+    // rest of arguments = num1
+        return num1;
+    
+    }
+    
+console.log(calculateCartPrice(200,400,600,800));
+
+const user={
+name: "areeba",
+age :21
+}
+
+function handleObjet(anyobject){
+    console.log(`username is ${anyobject.name} and age is ${anyobject.age}`);
+
+}
+
+// handleObjet(user);
+// directly passing an object 
+ handleObjet({
+    name:"shehroze",
+    age :21
+ })
+
+
+ // ========== function with arrays===========
+
+ const array1=[200,300,400];
+
+ function returnSecondValue(getArray){
+    return getArray[1];
+
+ }
+
+ const value = returnSecondValue(array1);
+ console.log(value);
+ // directky passing 
+ console.log(returnSecondValue([1,2,34,4]))
+
+
+
+ // ================funstions and  scope ==============
+
+ let a =10// block scope
+ const b =20
+ {
+ var c=30// global scope 
+ }
+
+ console.log(a);
+ console.log(b);
+ console.log(c);
+
+
+
