693cc5d18a9fcf215d71afd96335cec4190ca824
Areeba Naeem <areebanaeem069@gmail.com>
2025-01-11 03:21:03 +0500

    Update loops_basics.js

--- loops/loops_basics.js
+++ loops/loops_basics.js
@@ -38,3 +38,28 @@ console.log(element,"Others")
 
     
 }
+
+
+// while
+ index=0;
+ while (index<10){
+    console.log(`my value is ${index}`);
+    index++;
+
+ }
+index=0
+ while(index<array.length){
+    console.log(`array index :${index} and value ${array[index]}`);
+index++
+ }
+
+//  do while 
+average=30
+score=[50,33,20]
+i=0
+do {
+    console.log("pass");
+    i++;
+
+    
+}while (score[i]>average);
