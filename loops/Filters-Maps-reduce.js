02121b8e8e710080c5bf955ed6f8e5135358db9f
Areeba Naeem <areebanaeem069@gmail.com>
2025-01-11 22:42:01 +0500

    Update Filters-Maps-reduce.js

--- loops/Filters-Maps-reduce.js
+++ loops/Filters-Maps-reduce.js
@@ -21,8 +21,75 @@ console.log(newnums2)
 const new1=myNums.forEach((num)=>num+10)
 console.log(new1) //doesnot return (undefined return )
 
-
+// map can return a value 
 const neww=myNums.map((num)=>{return num+10}
 )
  console.log(neww)
+
+
+
+//   chaining of methods 
+// works like nested loops
+const newNUms=myNums
+                .map((num)=>num*10 )
+                .map((num)=>num+1)
+                .filter((num)=>num>-40);
+
+console.log(newNUms);
+
+// ===============reduce =================
+//  jo bhi intial value dai gy first iteration mai accumulator mai wohi jay gi 
+
+const array1=[1,2,3]
+const total=array1.reduce(function(acc,currentval){
+    console.log(`acc:${acc} currentvalue: ${currentval}`);
+
+    return acc+currentval;
+
+},0)
+
+console.log(total);
+
+//  shorter way 
+const totalvalues=array1.reduce(
+    (acc,current)=> acc+current,0
+)
+
+console.log(totalvalues);
+
+// ===============shopping cart example==============
+const shoppingCart=[
+    {
+        itemName:"Js course",
+        price :2999
+    },
+    {
+        itemName:"Python course",
+        price :999
+    },
+    {
+        itemName:"Mobile app course",
+        price :10000
+    }
+]
+
+const totalResult=shoppingCart.reduce(
+   function (acc,item){
+    console.log(item.price);
+    return item.price+acc
+
+
+    },0
+)
+
+const pricee=shoppingCart.reduce(
+    (acc,current)=>
+        {
+
+            return acc+current.price
+        
+        },0
+)
+console.log(totalResult)
+console.log(pricee)
 
